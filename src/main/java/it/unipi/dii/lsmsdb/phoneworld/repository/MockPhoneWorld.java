58bd9376bd97330bcc7e9019807b9306ac437f09
FrankMartoccia <francescomartoccia3@gmail.com>
2021-12-23 18:58:17 +0100

    Add initMock().

--- src/main/java/it/unipi/dii/lsmsdb/phoneworld/repository/MockPhoneWorld.java
+++ src/main/java/it/unipi/dii/lsmsdb/phoneworld/repository/MockPhoneWorld.java
@@ -1,5 +1,35 @@
 package it.unipi.dii.lsmsdb.phoneworld.repository;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import it.unipi.dii.lsmsdb.phoneworld.model.Phone;
+import it.unipi.dii.lsmsdb.phoneworld.model.Review;
+import it.unipi.dii.lsmsdb.phoneworld.model.User;
+
+import org.bson.Document;
+
 public class MockPhoneWorld {
+
+    public MockPhoneWorld() {
+    }
+
+    public void initMock() {
+        ConnectionString uri = new ConnectionString("mongodb://localhost:27017/testPhoneWorld");
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase db = mongoClient.getDatabase("testPhoneWorld");
+        MongoCollection<Document> phones = db.getCollection("Phones");
+        MongoCollection<Document> reviews = db.getCollection("Reviews");
+        MongoCollection<Document> users = db.getCollection("Users");
+        //db.getCollection("Phones").drop(); TODO in test
+        //db.getCollection("Reviews").drop(); TODO in test
+        //db.getCollection("Users").drop(); TODO in test
+    }
+
+    public void initUsers() {
+        //Document user = new Document("")
+    }
 
 }

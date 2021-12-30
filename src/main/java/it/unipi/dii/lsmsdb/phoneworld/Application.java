9d87b99f66147c692b9f7eb33c5a00e540dab887
FrankMartoccia <francescomartoccia3@gmail.com>
2021-12-31 00:10:23 +0100

    Add JAVAFX support.

--- src/main/java/it/unipi/dii/lsmsdb/phoneworld/Application.java
+++ src/main/java/it/unipi/dii/lsmsdb/phoneworld/Application.java
@@ -1,88 +1,89 @@
 package it.unipi.dii.lsmsdb.phoneworld;
 
+import javafx.stage.Stage;
 import it.unipi.dii.lsmsdb.phoneworld.model.ModelBean;
 import it.unipi.dii.lsmsdb.phoneworld.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import javax.swing.*;
 
 //@ComponentScan("it.unipi.dii.lsmsdb.*")
 //@EntityScan("it.unipi.dii.lsmsdb.*")
 //@EnableMongoRepositories("it.unipi.dii.lsmsdb.*")
 @SpringBootApplication
-public class Application {
+public class Application extends javafx.application.Application {
 
-	@Autowired
-	private UserMongo userMongo;
+    @Autowired
+    private UserMongo userMongo;
 
-	@Autowired
-	private PhoneMongo phoneMongo;
+    @Autowired
+    private PhoneMongo phoneMongo;
 
-	@Autowired
-	private ReviewMongo reviewMongo;
-	private final GraphNeo4j graphNeo4j = new GraphNeo4j("bolt://localhost:7687",
-                    "neo4j", "PhoneWorld");
-	private final UserNeo4j userNeo4j = new UserNeo4j(graphNeo4j);
-	private final PhoneNeo4j phoneNeo4j = new PhoneNeo4j(graphNeo4j);
+    @Autowired
+    private ReviewMongo reviewMongo;
 
-	private static final Application singleton = new Application();
-	private final ModelBean modelBean = new ModelBean();
+    private GraphNeo4j graphNeo4j;
+    private UserNeo4j userNeo4j;
+    private PhoneNeo4j phoneNeo4j;
+    private ModelBean modelBean;
 
-	public static Application getInstance() {
-		return singleton;
-	}
+    private static final Application singleton = new Application();
 
-	public ModelBean getModelBean() {
-		return modelBean;
-	}
+    public static Application getInstance() {
+        return singleton;
+    }
 
-	public UserMongo getUserMongo() {
-		return userMongo;
-	}
+    public ModelBean getModelBean() {
+        return modelBean;
+    }
 
-	public PhoneMongo getPhoneMongo() {
-		return phoneMongo;
-	}
+    public UserMongo getUserMongo() {
+        return userMongo;
+    }
 
-	public ReviewMongo getReviewMongo() {
-		return reviewMongo;
-	}
+    public PhoneMongo getPhoneMongo() {
+        return phoneMongo;
+    }
 
-	public GraphNeo4j getGraphNeo4j() {
-		return graphNeo4j;
-	}
+    public ReviewMongo getReviewMongo() {
+        return reviewMongo;
+    }
 
-	public UserNeo4j getUserNeo4j() {
-		return userNeo4j;
-	}
+    public GraphNeo4j getGraphNeo4j() {
+        return graphNeo4j;
+    }
 
-	public PhoneNeo4j getPhoneNeo4j() {
-		return phoneNeo4j;
-	}
+    public UserNeo4j getUserNeo4j() {
+        return userNeo4j;
+    }
 
-	public static Application getSingleton() {
-		return singleton;
-	}
+    public PhoneNeo4j getPhoneNeo4j() {
+        return phoneNeo4j;
+    }
 
-	private void init() {
+    private void initApp() {
+        this.modelBean = new ModelBean();
+        this.graphNeo4j = new GraphNeo4j("bolt://localhost:7687",
+                "neo4j", "PhoneWorld");
+        this.userNeo4j = new UserNeo4j(graphNeo4j);
+        this.phoneNeo4j = new PhoneNeo4j(graphNeo4j);
+    }
 
-	}
+    /*To test on the real database
 
-// To test on the real database
+        @EventListener(ApplicationReadyEvent.class)
+        public void afterTheStart() {
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void afterTheStart() {
+        }
+    */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        Application.getInstance().initApp();
+    }
 
-	}
+    @Override
+    public void start(Stage primaryStage) {
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			SpringApplication.run(Application.class, args);
-			Application.getInstance().init();
-		});
-	}
+    }
+
 }
